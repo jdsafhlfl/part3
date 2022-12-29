77cd409ea3980f77b9da7d3e73973e3fdfcfe67a
jdsafhlfl <weiyi.xu@helsinki.fi>
2022-12-29 18:43:00 +0200

    exercise 3.15

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -13,9 +13,6 @@ app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :add'))
 app.use(express.static('build'))
 
-let data = [
-
-]
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons =>{
@@ -40,10 +37,12 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response)=>{
-    const id = request.params.id
-    data = data.filter(p => p.id !== Number(id))
+    Person.findByIdAndDelete(request.params.id)
+          .then(result =>{
+            response.status(204).end()
+          })
+          .catch(error => next(error) )
 
-    response.status(204).end()
 })
 
 app.post('/api/persons', (request, response)=>{
